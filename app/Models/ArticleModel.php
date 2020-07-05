b3cd7e3394c4729936a9e2c63084b8a0db4bfbac
azdyf <azdy.fm@gmail.com>
2020-07-05 16:26:20 +0700

    3. Membuat Model Artikel

---
+++ app/Models/ArticleModel.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Carbon\Carbon;
+
+class ArticleModel
+{
+    // Create
+    public static function create($request)
+    {
+
+        $slug = Str::slug($request['title'], '-');
+
+        return DB::table('articles')->insert([
+            'title' => $request['title'],
+            'content' => $request['content'],
+            'slug' => $slug,
+            'user_id' => 0,
+            'created_at' => Carbon::now(),
+        ]);
+    }
+
+    // Read
+    public static function getAll()
+    {
+        return DB::table('articles')->get();
+    }
+
+    // Update
+    public static function update($id, $request)
+    {
+        $slug = Str::slug($request['title'], '-');
+
+        return DB::table('articles')
+            ->where('id', $id)
+            ->update([
+                'title' => $request['title'],
+                'content' => $request['content'],
+                'slug' => $slug,
+                'updated_at' => Carbon::now(),
+            ]);
+    }
+
+    // Delete
+    public static function destroy($id)
+    {
+        return DB::table('articles')
+            ->where('id', $id)
+            ->delete($id);
+    }
+}
